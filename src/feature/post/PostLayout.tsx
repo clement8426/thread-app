f049b3cf6ca572fe27d3e956db981e2702602d10
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-23 12:59:33 +0100

    ll

--- src/feature/post/PostLayout.tsx
+++ src/feature/post/PostLayout.tsx
@@ -1,6 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { formatDate } from "@/lib/date";
 import { PostHome } from "@/src/query/post.query";
 import clsx from "clsx";
+import { MoreHorizontal } from "lucide-react";
+import Link from "next/link";
 import { PropsWithChildren } from "react";
 
 type PostLayoutProps = PropsWithChildren<{
@@ -26,6 +29,21 @@ export const PostLayout = ({
           {user.username.slice(0, 2).toUpperCase()}
         </AvatarFallback>
       </Avatar>
+      <div className="ml-4 flex w-full flex-col gap-2">
+        <Link href={`/users/${user.id}`}>
+          <div className="flex flex-row items-center gap-2">
+            <p className="text-sm text-card-foreground mr-auto">
+              {user.username}
+            </p>
+            {createdAt ? (
+              <p className="text-sm text-muted-foreground">
+                {formatDate(createdAt)}
+              </p>
+            ) : null}
+            <MoreHorizontal size={20} />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
