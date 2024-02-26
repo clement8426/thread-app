05cbff3c1c745df1ad007f155870c2eaae70abf3
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-26 16:24:43 +0100

    ll

---
+++ app/@modal/(.)write/WriteModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import WritePostFrom, { WritePostFormValues } from "@/app/write/WritePostForm";
+import { createPost } from "@/app/write/write-post.action";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { User } from "@prisma/client";
+import { usePathname, useRouter } from "next/navigation";
+import React from "react";
+
+export default function WriteModal({
+  user,
+  createPost,
+}: {
+  user: User;
+  createPost: (values: WritePostFormValues) => Promise<string>;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <Dialog
+      open={pathname === "/write"}
+      onOpenChange={() => {
+        router.back();
+      }}
+    >
+      <DialogContent>
+        <WritePostFrom user={user} onSubmit={createPost} />
+      </DialogContent>
+    </Dialog>
+  );
+}
