f825fef4d02dd9c33b8914c76f82a18758eb6b6f
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-28 12:52:56 +0100

    ll

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,8 +1,30 @@
+import { getAuthSession } from "@/lib/auth";
 import WritePostFrom from "./WritePostForm";
 import { createPost } from "./write-post.action";
 import { getUser } from "@/src/query/user.query";
+import { signIn } from "next-auth/react";
+import { prisma } from "@/lib/prisma";
 
-export default async function Write() {
-  const user = await getUser();
-  return <WritePostFrom user={user} onSubmit={createPost} />;
+export default async function page() {
+  const session = await getAuthSession();
+
+  if (!session?.user.id) {
+    await signIn();
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: session?.user.id,
+    },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  return (
+    <div className="w-full">
+      <WritePostFrom user={user} onSubmit={createPost} />
+    </div>
+  );
 }
