b9954bf7a9709ea7ad1484b4b7ebb4c6b8d3a793
clement bastien bastienclement826@gmail.com <bastienclement826@gmail.com>
2024-02-28 13:25:45 +0100

    connect

--- app/connect/page.tsx
+++ app/connect/page.tsx
@@ -4,18 +4,7 @@ import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertTriangle } from "lucide-react";
 import { useEffect } from "react";
 
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
+export default function Connect() {
   return (
     <Alert className="my-8">
       <AlertTriangle />
